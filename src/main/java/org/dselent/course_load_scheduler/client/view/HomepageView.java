3c1e8a9edaba36a668d8662f052bbb2f71476679
drd387 <dannyd40@gmail.com>
2018-02-12 21:21:46 -0500

    committing source files

---
+++ src/main/java/org/dselent/course_load_scheduler/client/view/HomepageView.java
@@ -0,0 +1,8 @@
+package org.dselent.course_load_scheduler.client.view;
+
+import org.dselent.course_load_scheduler.client.presenter.HomepagePresenter;
+
+public interface HomepageView extends BaseView<HomepagePresenter>{
+	
+	
+}
