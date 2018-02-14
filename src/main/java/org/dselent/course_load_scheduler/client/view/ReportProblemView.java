3243e822ea7fc5b92de189756d5c70ec2e2558f9
bobbyshome <bobbyshome@autoreg-070211.dyn.wpi.edu>
2018-02-14 09:18:51 -0500

    Adding UiFields

--- src/main/java/org/dselent/course_load_scheduler/client/view/ReportProblemView.java
+++ src/main/java/org/dselent/course_load_scheduler/client/view/ReportProblemView.java
@@ -2,10 +2,24 @@ package org.dselent.course_load_scheduler.client.view;
 
 import org.dselent.course_load_scheduler.client.presenter.ReportProblemPresenter;
 
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HasWidgets;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.TextArea;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
 
 public interface ReportProblemView extends BaseView<ReportProblemPresenter> {
 	
+	public ListBox getTypeList();
+	public void setTypeList(ListBox typeList);
+	public TextBox getNameBox();
+	public void setNameBox(TextBox nameBox);
+	public TextBox getEmailBox();
+	public void setEmailBox(TextBox emailBox);
+	public TextArea getDescriptionArea();
+	public void setDescriptionArea(TextArea descriptionArea);
+	public Button getExitButton();
+	public Button getSubmitButton();
 	
 }
--- src/main/java/org/dselent/course_load_scheduler/client/view/impl/ReportProblemViewImpl.java
+++ src/main/java/org/dselent/course_load_scheduler/client/view/impl/ReportProblemViewImpl.java
@@ -9,19 +9,75 @@ import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HasWidgets;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.LayoutPanel;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.TextArea;
 
 public class ReportProblemViewImpl extends BaseViewImpl<ReportProblemPresenter> implements ReportProblemView {
 
 	private static ReportProblemViewImplUiBinder uiBinder = GWT.create(ReportProblemViewImplUiBinder.class);
+	@UiField HTMLPanel basePanel;
+	@UiField Label titleLabel;
+	@UiField Button exitButton;
+	@UiField LayoutPanel layoutPanel;
+	@UiField ListBox typeList;
+	@UiField TextBox nameBox;
+	@UiField TextBox emailBox;
+	@UiField TextArea descriptionArea;
+	@UiField Button submitButton;
 
 	interface ReportProblemViewImplUiBinder extends UiBinder<Widget, ReportProblemViewImpl> {
 	}
 
 	public ReportProblemViewImpl() {
 		initWidget(uiBinder.createAndBindUi(this));
+	}
+
+	public ListBox getTypeList() {
+		return typeList;
+	}
+
+	public void setTypeList(ListBox typeList) {
+		this.typeList = typeList;
+	}
+
+	public TextBox getNameBox() {
+		return nameBox;
+	}
+
+	public void setNameBox(TextBox nameBox) {
+		this.nameBox = nameBox;
+	}
+
+	public TextBox getEmailBox() {
+		return emailBox;
+	}
+
+	public void setEmailBox(TextBox emailBox) {
+		this.emailBox = emailBox;
+	}
+
+	public TextArea getDescriptionArea() {
+		return descriptionArea;
+	}
+
+	public void setDescriptionArea(TextArea descriptionArea) {
+		this.descriptionArea = descriptionArea;
+	}
+
+	public Button getExitButton() {
+		return exitButton;
+	}
+
+	public Button getSubmitButton() {
+		return submitButton;
 	}
 
 	@Override
@@ -37,7 +93,17 @@ public class ReportProblemViewImpl extends BaseViewImpl<ReportProblemPresenter> 
 
 	@Override
 	public HasWidgets getViewRootPanel() {
-		// TODO Auto-generated method stub
-		return null;
+		return basePanel;
 	}
+	
+	@UiHandler("exitButton")
+	void onExitButtonClicked() {
+		//add to this during event handling stage
+	}
+	
+	@UiHandler("submitButton")
+	void onSubmitButtonClicked() {
+		//add to this during event handling stage
+	}
+	
 }
