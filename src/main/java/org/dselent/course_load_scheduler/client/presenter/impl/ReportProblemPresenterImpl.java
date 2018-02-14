2755a3279b388ac23d979fd865a8a3e6d7cd85b4
bobbyshome <bobbyshome@autoreg-070211.dyn.wpi.edu>
2018-02-13 21:36:20 -0500

    presenterImpl

---
+++ src/main/java/org/dselent/course_load_scheduler/client/presenter/impl/ReportProblemPresenterImpl.java
@@ -0,0 +1,53 @@
+package org.dselent.course_load_scheduler.client.presenter.impl;
+
+import javax.inject.Inject;
+
+import org.dselent.course_load_scheduler.client.model.Model;
+import org.dselent.course_load_scheduler.client.presenter.BasePresenter;
+import org.dselent.course_load_scheduler.client.presenter.ReportProblemPresenter;
+import org.dselent.course_load_scheduler.client.view.BaseView;
+import org.dselent.course_load_scheduler.client.view.ReportProblemView;
+
+import com.google.gwt.user.client.ui.HasWidgets;
+
+public class ReportProblemPresenterImpl extends BasePresenterImpl implements ReportProblemPresenter {
+	
+	private ReportProblemView view;
+	
+	@Inject
+	public ReportProblemPresenterImpl(ReportProblemView view) {
+		this.view = view;
+		view.setPresenter(this);
+	}
+
+	@Override
+	public void go(HasWidgets container) {
+		container.clear();
+		container.add(view.getWidgetContainer());
+
+	}
+
+	@Override
+	public ReportProblemView getView() {
+		return view;
+	}
+	
+	@Override
+	public Model getModel()
+	{
+		return null;
+	}
+
+	@Override
+	public void showLoadScreen() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void hideLoadScreen() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
