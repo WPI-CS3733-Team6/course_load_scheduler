3edf740872aa18751ac157a594a7d17a42e85d80
Javier Marcos <xbc30ep450@gmail.com>
2018-02-14 15:56:37 -0500

    Created "AdminCourseSearchImpl" UI files

---
+++ src/main/java/org/dselent/course_load_scheduler/client/view/impl/AdminCourseSearchImpl.java
@@ -0,0 +1,36 @@
+package org.dselent.course_load_scheduler.client.view.impl;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HasText;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.AbsolutePanel;
+import com.google.gwt.user.client.ui.LayoutPanel;
+import com.google.gwt.user.client.ui.FlexTable;
+
+public class AdminCourseSearchImpl extends Composite{
+
+	private static AdminCourseSearchImplUiBinder uiBinder = GWT.create(AdminCourseSearchImplUiBinder.class);
+	@UiField ListBox semesterList;
+	@UiField ListBox courseList;
+	@UiField Button searchButton;
+	@UiField Button advSearchButton;
+	@UiField Button addCourseButton;
+	@UiField LayoutPanel layoutPanel;
+	@UiField FlexTable resultTable;
+
+	interface AdminCourseSearchImplUiBinder extends UiBinder<Widget, AdminCourseSearchImpl> {
+	}
+
+	public AdminCourseSearchImpl() {
+		initWidget(uiBinder.createAndBindUi(this));
+	}
+
+}
