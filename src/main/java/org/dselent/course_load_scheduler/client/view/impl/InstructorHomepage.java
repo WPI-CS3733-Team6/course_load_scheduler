880ede8e5dc90aef719ec58ce186ddbc9946439c
Josue Contreras <josudaco@hotmail.com>
2018-02-15 02:55:49 -0500

    Instructor Homepage

---
+++ src/main/java/org/dselent/course_load_scheduler/client/view/impl/InstructorHomepage.java
@@ -0,0 +1,27 @@
+package org.dselent.course_load_scheduler.client.view.impl;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.event.dom.client.ClickEvent;
+
+public class InstructorHomepage extends Composite {
+
+	private static InstructorHomepageUiBinder uiBinder = GWT.create(InstructorHomepageUiBinder.class);
+	@UiField Button button;
+
+	interface InstructorHomepageUiBinder extends UiBinder<Widget, InstructorHomepage> {
+	}
+
+	public InstructorHomepage() {
+		initWidget(uiBinder.createAndBindUi(this));
+	}
+
+	@UiHandler("button")
+	void onButtonClick(ClickEvent event) {
+	}
+}
