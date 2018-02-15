481bb1cac90045c0714c537fd188a141b8aa67ff
drd387 <dannyd40@gmail.com>
2018-02-14 19:24:25 -0500

    added needed methods for AccoutInfoPresenterImpl

--- src/main/java/org/dselent/course_load_scheduler/client/presenter/impl/AccountInfoPresenterImpl.java
+++ src/main/java/org/dselent/course_load_scheduler/client/presenter/impl/AccountInfoPresenterImpl.java
@@ -1,12 +1,11 @@
 package org.dselent.course_load_scheduler.client.presenter.impl;
 
+import org.dselent.course_load_scheduler.client.event.InvalidLoginEvent;
 import org.dselent.course_load_scheduler.client.presenter.AccountInfoPresenter;
-import org.dselent.course_load_scheduler.client.presenter.BasePresenter;
 import org.dselent.course_load_scheduler.client.presenter.IndexPresenter;
 import org.dselent.course_load_scheduler.client.view.AccountInfoView;
-import org.dselent.course_load_scheduler.client.view.BaseView;
-import org.dselent.course_load_scheduler.client.view.LoginView;
 
+import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.ui.HasWidgets;
 import com.google.inject.Inject;
 
@@ -14,6 +13,8 @@ public class AccountInfoPresenterImpl extends BasePresenterImpl implements Accou
 	
 	private IndexPresenter parentPresenter;
 	private AccountInfoView view;
+	//don't know what clickInProgress means, but it's here because it's in LoginPresenterImpl
+	private boolean accountInfoClickInProgress;
 	
 	@Inject
 	public AccountInfoPresenterImpl(IndexPresenter parentPresenter, AccountInfoView view)
@@ -21,36 +22,51 @@ public class AccountInfoPresenterImpl extends BasePresenterImpl implements Accou
 		this.view = view;
 		this.parentPresenter = parentPresenter;
 		view.setPresenter(this);
+		accountInfoClickInProgress = false;
 	}
 	
 	@Override
-	public void go(HasWidgets container) {
-		// TODO Auto-generated method stub
+	public void init()
+	{
+		bind();
+	}
+
+	@Override
+	public void bind()
+	{
+		HandlerRegistration registration;
 		
+		registration = eventBus.addHandler(InvalidLoginEvent.TYPE, this);
+		eventBusRegistration.put(InvalidLoginEvent.TYPE, registration);
 	}
-
-	@Override
-	public BaseView<? extends BasePresenter> getView() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public IndexPresenter getParentPresenter() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void setParentPresenter(IndexPresenter parentPresenter) {
-		// TODO Auto-generated method stub
 		
+	@Override
+	public void go(HasWidgets container)
+	{
+		container.clear();
+		container.add(view.getWidgetContainer());
 	}
 
 	@Override
+	public AccountInfoView getView()
+	{
+		return view;
+	}
+	
+	@Override
+	public IndexPresenter getParentPresenter()
+	{
+		return parentPresenter;
+	}
+
+	@Override
+	public void setParentPresenter(IndexPresenter parentPresenter)
+	{
+		this.parentPresenter = parentPresenter;
+	}
+	
 	public void AccountInfo() {
-		// TODO Auto-generated method stub
-		
+		//TODO: all of whatever this is, as well as any other methods that have to happen here
 	}
 
 }
