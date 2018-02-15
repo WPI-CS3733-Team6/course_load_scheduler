3cbea6acade5662215778225ae4a192331b0a448
drd387 <dannyd40@gmail.com>
2018-02-14 20:46:20 -0500

    added getters to view interface

--- src/main/java/org/dselent/course_load_scheduler/client/view/AccountInfoView.java
+++ src/main/java/org/dselent/course_load_scheduler/client/view/AccountInfoView.java
@@ -2,6 +2,15 @@ package org.dselent.course_load_scheduler.client.view;
 
 import org.dselent.course_load_scheduler.client.presenter.AccountInfoPresenter;
 
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.PasswordTextBox;
+import com.google.gwt.user.client.ui.TextBox;
+
 public interface AccountInfoView extends BaseView<AccountInfoPresenter>{
-	
+	Button getSubmitChangesButton();
+	PasswordTextBox getCurrPassword();
+	PasswordTextBox getNewPassword();
+	PasswordTextBox getRepeatNewPassword();
+	TextBox getPreferedEmail();
+	TextBox getPhoneNumber();
 }
