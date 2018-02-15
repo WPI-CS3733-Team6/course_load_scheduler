4d334331c0fb8046db2bab4e101bfdcbd41124aa
aantaya1 <31359424+aantaya1@users.noreply.github.com>
2018-02-15 18:18:35 -0500

    Updated CurrentCoursesView and impl

--- src/main/java/org/dselent/course_load_scheduler/client/view/impl/ViewCurrentClassesViewImpl.java
+++ src/main/java/org/dselent/course_load_scheduler/client/view/impl/ViewCurrentClassesViewImpl.java
@@ -6,15 +6,12 @@ import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.LayoutPanel;
-import com.google.gwt.user.client.ui.DecoratedTabBar;
-import com.google.gwt.user.client.ui.TabPanel;
 
 public class ViewCurrentClassesViewImpl extends Composite {
 
 	private static ViewCurrentClassesViewImplUiBinder uiBinder = GWT.create(ViewCurrentClassesViewImplUiBinder.class);
-	@UiField LayoutPanel basePanelViewCurrentClasses;
-	@UiField TabPanel SwitchBetweenTabBar;
-	@UiField LayoutPanel SimpleCurrentClasses;
+	@UiField LayoutPanel CurrentCourses;
+	@UiField LayoutPanel DetailedCurrentCourses;
 
 	interface ViewCurrentClassesViewImplUiBinder extends UiBinder<Widget, ViewCurrentClassesViewImpl> {
 	}
