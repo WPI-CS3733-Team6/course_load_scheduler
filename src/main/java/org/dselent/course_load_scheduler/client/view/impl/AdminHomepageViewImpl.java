425ba3b938e534b782b8068dd9689813747c0094
Josue Contreras <josudaco@hotmail.com>
2018-02-16 18:29:14 -0500

    small changes

--- src/main/java/org/dselent/course_load_scheduler/client/view/impl/AdminHomepageViewImpl.java
+++ src/main/java/org/dselent/course_load_scheduler/client/view/impl/AdminHomepageViewImpl.java
@@ -14,12 +14,40 @@ import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.TabPanel;
 import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.DecoratedTabPanel;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.SplitLayoutPanel;
+import com.google.gwt.user.client.ui.HorizontalPanel;
 
 public class AdminHomepageViewImpl extends Composite {
 
 	private static AdminHomepageViewImplUiBinder uiBinder = GWT.create(AdminHomepageViewImplUiBinder.class);
 	@UiField LayoutPanel layoutPanel;
 	@UiField HTMLPanel adminHomepageView;
+	@UiField LayoutPanel viewAsAdministrator;
+	@UiField LayoutPanel viewAsInstructor;
+	@UiField ListBox department;
+	@UiField TextBox departmentName;
+	@UiField ListBox courses;
+	@UiField Button addUser;
+	@UiField Button editUser;
+	@UiField Button addCourse;
+	@UiField Button modifyCourse;
+	@UiField TextBox courseText;
+	@UiField LayoutPanel layoutPanelInstructor;
+	@UiField Button createRequest;
+	@UiField Button generatedCompleteDepartmentSchedule;
+	@UiField LayoutPanel courseSummariesPanel;
+	@UiField SplitLayoutPanel table;
+	@UiField HorizontalPanel north;
+	@UiField ListBox west;
+	@UiField ListBox center;
+	@UiField TextBox courseSummaries;
+	@UiField HorizontalPanel horizontalPanel;
+	@UiField Button currentClassesButton;
+	@UiField Button searchClassesButton;
+	@UiField Button accountInfoButton;
+	@UiField Button courseRegistrationCart;
 
 	interface AdminHomepageViewImplUiBinder extends UiBinder<Widget, AdminHomepageViewImpl> {
 	}
