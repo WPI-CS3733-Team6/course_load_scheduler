2333483926a73bc52a23c319e0adc757e494beb3
Avorent <panlau98@gmail.com>
2018-02-15 20:36:58 -0500

    SearchClasses

--- src/main/java/org/dselent/course_load_scheduler/client/view/impl/SearchClassesViewImpl.java
+++ src/main/java/org/dselent/course_load_scheduler/client/view/impl/SearchClassesViewImpl.java
@@ -4,10 +4,21 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.event.dom.client.ClickEvent;
 
 public class SearchClassesViewImpl extends Composite {
 
 	private static SearchClassesViewImplUiBinder uiBinder = GWT.create(SearchClassesViewImplUiBinder.class);
+	@UiField ListBox Semester;
+	@UiField Button button;
+	@UiField ListBox Subject;
+	@UiField ListBox Course_Name;
+	@UiField ListBox Registration_Status;
+	@UiField ListBox Course_Number;
 
 	interface SearchClassesViewImplUiBinder extends UiBinder<Widget, SearchClassesViewImpl> {
 	}
@@ -16,4 +27,7 @@ public class SearchClassesViewImpl extends Composite {
 		initWidget(uiBinder.createAndBindUi(this));
 	}
 
+	@UiHandler("button")
+	void onButtonClick(ClickEvent event) {
+	}
 }
