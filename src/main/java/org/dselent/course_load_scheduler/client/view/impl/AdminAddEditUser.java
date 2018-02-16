d3a3e61c0b2d44b541246e582d4d376ed57d48f3
Josue Contreras <josudaco@hotmail.com>
2018-02-15 19:48:35 -0500

    Administrator add/edit user & homepage

---
+++ src/main/java/org/dselent/course_load_scheduler/client/view/impl/AdminAddEditUser.java
@@ -0,0 +1,24 @@
+package org.dselent.course_load_scheduler.client.view.impl;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Button;
+
+public class AdminAddEditUser extends Composite {
+
+	private static AdminAddEditUserUiBinder uiBinder = GWT.create(AdminAddEditUserUiBinder.class);
+	@UiField Button home;
+	@UiField Button logout;
+	@UiField Button admin;
+
+	interface AdminAddEditUserUiBinder extends UiBinder<Widget, AdminAddEditUser> {
+	}
+
+	public AdminAddEditUser() {
+		initWidget(uiBinder.createAndBindUi(this));
+	}
+
+}
--- src/main/java/org/dselent/course_load_scheduler/client/view/impl/AdminHomepageViewImpl.java
+++ src/main/java/org/dselent/course_load_scheduler/client/view/impl/AdminHomepageViewImpl.java
@@ -8,20 +8,23 @@ import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.user.client.ui.LayoutPanel;
+import com.google.gwt.user.client.ui.AbsolutePanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TabPanel;
+import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.DecoratedTabPanel;
 
 public class AdminHomepageViewImpl extends Composite {
 
 	private static AdminHomepageViewImplUiBinder uiBinder = GWT.create(AdminHomepageViewImplUiBinder.class);
-	@UiField Button button;
+	@UiField LayoutPanel layoutPanel;
+	@UiField HTMLPanel adminHomepageView;
 
 	interface AdminHomepageViewImplUiBinder extends UiBinder<Widget, AdminHomepageViewImpl> {
 	}
 
 	public AdminHomepageViewImpl() {
 		initWidget(uiBinder.createAndBindUi(this));
-	}
-
-	@UiHandler("button")
-	void onButtonClick(ClickEvent event) {
 	}
 }
