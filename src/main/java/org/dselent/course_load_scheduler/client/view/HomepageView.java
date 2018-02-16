61d0ca66004e9ed7705bfa8d0de5427e5f26c717
drd387 <dannyd40@gmail.com>
2018-02-16 18:08:18 -0500

    started work on course registration cart

--- src/main/java/org/dselent/course_load_scheduler/client/view/HomepageView.java
+++
@@ -1,7 +0,0 @@
-package org.dselent.course_load_scheduler.client.view;
-
-import org.dselent.course_load_scheduler.client.presenter.HomepagePresenter;
-
-public interface HomepageView extends BaseView<HomepagePresenter>{	
-	
-}
