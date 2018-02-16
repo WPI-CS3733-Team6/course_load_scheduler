f93fce3fcdbe8494b5fde26de89363033ce20ce2
Avorent <panlau98@gmail.com>
2018-02-15 21:55:12 -0500

    changed parameter names to fit java's naming scheme
    DetailedCourseInformation

--- src/main/java/org/dselent/course_load_scheduler/client/view/impl/DetailedCourseInformationViewImpl.java
+++ src/main/java/org/dselent/course_load_scheduler/client/view/impl/DetailedCourseInformationViewImpl.java
@@ -6,19 +6,22 @@ import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.Button;
 
 public class DetailedCourseInformationViewImpl extends Composite {
 
 	private static DetailedCourseInformationViewImplUiBinder uiBinder = GWT
 			.create(DetailedCourseInformationViewImplUiBinder.class);
-	@UiField TextBox Registration_Status;
-	@UiField TextBox Course_Name;
-	@UiField TextBox Course_Number;
-	@UiField TextBox Level;
-	@UiField TextBox Section_Id;
-	@UiField TextBox Section_Type;
-	@UiField TextBox Schedule_Days;
+	@UiField TextBox registrationStatus;
+	@UiField TextBox courseName;
+	@UiField TextBox courseNumber;
+	@UiField TextBox level;
+	@UiField TextBox sectionId;
+	@UiField TextBox sectionType;
+	@UiField TextBox scheduleDays;
 	@UiField TextBox description;
+	@UiField TextBox expectedPop;
+	@UiField Button submitToCart;
 
 	interface DetailedCourseInformationViewImplUiBinder extends UiBinder<Widget, DetailedCourseInformationViewImpl> {
 	}
