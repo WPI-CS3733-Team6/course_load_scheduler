bcffe836d9f98424dc6d34b3ef2484d3bd2b31b1
bobbyshome <bobbyshome@autoreg-070211.dyn.wpi.edu>
2018-02-17 14:01:00 -0500

    InstructorHomepage update

---
+++ src/main/java/org/dselent/course_load_scheduler/client/presenter/InstructorHomepagePresenter.java
@@ -0,0 +1,12 @@
+package org.dselent.course_load_scheduler.client.presenter;
+
+public interface InstructorHomepagePresenter extends BasePresenter {
+	IndexPresenter getParentPresenter();
+	void setParentPresenter(IndexPresenter parentPresenter);
+	public void currentClasses();
+	public void classSearch();
+	public void accountInfo();
+	public void courseCart();
+	public void createRequest();
+	public void generateSchedule();
+}
