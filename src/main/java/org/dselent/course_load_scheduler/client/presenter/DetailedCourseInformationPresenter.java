0f8d11b0556d7c8b48e74933d1ad9cf8457908c6
Josue Contreras <josudaco@hotmail.com>
2018-02-16 20:07:58 -0500

    detailed course info presenter

---
+++ src/main/java/org/dselent/course_load_scheduler/client/presenter/DetailedCourseInformationPresenter.java
@@ -0,0 +1,8 @@
+package org.dselent.course_load_scheduler.client.presenter;
+
+public interface DetailedCourseInformationPresenter extends BasePresenter
+{
+	IndexPresenter getParentPresenter();
+	void setParentPresenter(IndexPresenter parentPresenter);
+	//maybe make a method to do something cool - Josue thinking...
+}
