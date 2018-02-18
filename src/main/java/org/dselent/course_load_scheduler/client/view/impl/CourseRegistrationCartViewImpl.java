af016e315dfbeb76691d10a44619391881989b66
Josue Contreras <josudaco@hotmail.com>
2018-02-17 23:56:23 -0500

    course registration

--- src/main/java/org/dselent/course_load_scheduler/client/view/impl/CourseRegistrationCartViewImpl.java
+++ src/main/java/org/dselent/course_load_scheduler/client/view/impl/CourseRegistrationCartViewImpl.java
@@ -1,20 +1,25 @@
 package org.dselent.course_load_scheduler.client.view.impl;
 
+import org.dselent.course_load_scheduler.client.presenter.CourseRegistrationCartPresenter;
+import org.dselent.course_load_scheduler.client.view.CourseRegistrationCartView;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
-import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HasWidgets;
 
-public class CourseRegistrationCartViewImpl extends Composite {
+public class CourseRegistrationCartViewImpl extends BaseViewImpl<CourseRegistrationCartPresenter> implements CourseRegistrationCartView{
+
 
 	private static CourseRegistrationCartUiBinder uiBinder = GWT.create(CourseRegistrationCartUiBinder.class);
+
 	@UiField Button submitForApproval;
 	@UiField Button removeCourse;
 	@UiField FlexTable flexTableFor;
-
+	
 	interface CourseRegistrationCartUiBinder extends UiBinder<Widget, CourseRegistrationCartViewImpl> {
 	}
 
@@ -51,5 +56,24 @@ public class CourseRegistrationCartViewImpl extends Composite {
 	public FlexTable getFlexTableFor() {
 		return flexTableFor;
 	}
+	
+
+	@Override
+	public void setPresenter(CourseRegistrationCartPresenter presenter) {
+		this.presenter = presenter;
+		
+	}
+
+	@Override
+	public Widget getWidgetContainer() {
+		return this;
+	}
+
+	@Override
+	public HasWidgets getViewRootPanel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 
 }
