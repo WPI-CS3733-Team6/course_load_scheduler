e51cde6c8290854f3d2bd567db628573ba41eb20
Panha <34078543+Avorent@users.noreply.github.com>
2018-02-19 21:35:15 -0500

    stuff

--- src/main/java/org/dselent/course_load_scheduler/client/event/SendLoginEvent.java
+++ src/main/java/org/dselent/course_load_scheduler/client/event/SendLoginEvent.java
@@ -4,36 +4,21 @@ import org.dselent.course_load_scheduler.client.action.SendLoginAction;
 import org.dselent.course_load_scheduler.client.event_handler.SendLoginEventHandler;
 
 import com.google.gwt.event.shared.GwtEvent;
+import com.google.gwt.user.client.ui.HasWidgets;
 
-public class SendLoginEvent extends GwtEvent<SendLoginEventHandler>
+
+public class SendLoginEvent extends DisplayEvent<SendLoginAction, SendLoginEventHandler>
 {
 	public static Type<SendLoginEventHandler> TYPE = new Type<SendLoginEventHandler>();
 	
-	private SendLoginAction action;
-	
-	public SendLoginEvent(SendLoginAction action)
-	{
-		this.action = action;
+	public SendLoginEvent(SendLoginAction action, HasWidgets container) {
+		super(action, container);
 	}
-	
-	public SendLoginAction getAction()
-	{
-		return action;
-	}
-	
-	/*
-	 * 
-	 */
 	@Override
 	public Type<SendLoginEventHandler> getAssociatedType()
 	{
 		return TYPE;
 	}
-
-	/*
-	 * 
-	 */
-	@Override
 	protected void dispatch(SendLoginEventHandler handler)
 	{
 		handler.onSendLogin(this);
