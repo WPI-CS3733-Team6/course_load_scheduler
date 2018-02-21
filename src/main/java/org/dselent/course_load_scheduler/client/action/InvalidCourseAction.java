cfe88d1e1ffae33f24929ef6891ea85ae098d39e
aantaya1 <31359424+aantaya1@users.noreply.github.com>
2018-02-20 19:54:34 -0500

    Added an event to the login presenter event bus
    And added list to invalid course event

--- src/main/java/org/dselent/course_load_scheduler/client/action/InvalidCourseAction.java
+++ src/main/java/org/dselent/course_load_scheduler/client/action/InvalidCourseAction.java
@@ -1,9 +1,27 @@
 package org.dselent.course_load_scheduler.client.action;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InvalidCourseAction {
 
+	private List<String> reasonList;
+	
 	public InvalidCourseAction() {
-		// TODO Auto-generated constructor stub
+		reasonList = new ArrayList<>();
 	}
-
+	
+	@Override
+	public String toString()
+	{
+		StringBuilder sb = new StringBuilder();
+		
+		for(String reason : reasonList)
+		{
+			sb.append(reason);
+			sb.append("\n");
+		}
+		
+		return sb.toString();
+	}
 }
--- src/main/java/org/dselent/course_load_scheduler/client/presenter/impl/LoginPresenterImpl.java
+++ src/main/java/org/dselent/course_load_scheduler/client/presenter/impl/LoginPresenterImpl.java
@@ -43,6 +43,7 @@ public class LoginPresenterImpl extends BasePresenterImpl implements LoginPresen
 		HandlerRegistration registration;
 		
 		registration = eventBus.addHandler(InvalidLoginEvent.TYPE, this);
+		registration = eventBus.addHandler(SendLoginEvent.TYPE, this); // Added this, but we are not sure of it is correct
 		eventBusRegistration.put(InvalidLoginEvent.TYPE, registration);
 		
 		//do we need to add registration for the 
