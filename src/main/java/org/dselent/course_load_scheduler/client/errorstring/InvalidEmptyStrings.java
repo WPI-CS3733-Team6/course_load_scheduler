9e9b8b012581b10fb5971c1f5f71bfb571d918a3
Josue Contreras <josudaco@hotmail.com>
2018-02-20 22:36:56 -0500

    Empty Parameter Exeption for everyone to use

---
+++ src/main/java/org/dselent/course_load_scheduler/client/errorstring/InvalidEmptyStrings.java
@@ -0,0 +1,12 @@
+package org.dselent.course_load_scheduler.client.errorstring;
+
+public abstract class InvalidEmptyStrings 
+{
+	public static final String NULL_EXPECTED_POPULATION = "Your expected population cannot be empty.";
+	public static final String NULL_TYPE = "Your type cannot be empty.";
+	public static final String NULL_DAYS = "Your days checkboxes cannot be empty";
+	public static final String NULL_START_MINUTES = "Your start minutes cannot be empty.";
+	public static final String NULL_START_HOURS = "Your start hours cannot be empty.";
+	public static final String NULL_END_MINUTES = "Your end minutes cannot be empty.";
+	public static final String NULL_END_HOURS = "Your end hours cannot be empty.";
+}
---
+++ src/main/java/org/dselent/course_load_scheduler/client/exceptions/EmptyParameterException.java
@@ -0,0 +1,10 @@
+package org.dselent.course_load_scheduler.client.exceptions;
+
+public class EmptyParameterException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+	
+	public EmptyParameterException() {
+		
+	}
+}
