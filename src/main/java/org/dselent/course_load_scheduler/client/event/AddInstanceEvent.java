78bea03907bbeef97e2753c536e1f3ca7f670a70
Panha <34078543+Avorent@users.noreply.github.com>
2018-02-20 21:50:11 -0500

    AddInstance changes

--- src/main/java/org/dselent/course_load_scheduler/client/event/AddInstanceEvent.java
+++ src/main/java/org/dselent/course_load_scheduler/client/event/AddInstanceEvent.java
@@ -3,36 +3,25 @@ package org.dselent.course_load_scheduler.client.event;
 import org.dselent.course_load_scheduler.client.action.AddInstanceAction;
 import org.dselent.course_load_scheduler.client.event_handler.AddInstanceEventHandler;
 
-import com.google.gwt.event.shared.GwtEvent;
+import com.google.gwt.user.client.ui.HasWidgets;
 
-public class AddInstanceEvent extends GwtEvent<AddInstanceEventHandler>{
+public class AddInstanceEvent extends DisplayEvent<AddInstanceAction, AddInstanceEventHandler>{
 	public static Type<AddInstanceEventHandler> TYPE = new Type<AddInstanceEventHandler>();
-
 	private AddInstanceAction action;
 
-	public AddInstanceEvent(AddInstanceAction action)
+	public AddInstanceEvent(AddInstanceAction action, HasWidgets container)
 	{
-		this.action = action;
+		super(action, container);
 	}
 
 	public AddInstanceAction getAction()
 	{
 		return action;
 	}
-
-	/*
-	 * 
-	 */
-	@Override
 	public Type<AddInstanceEventHandler> getAssociatedType()
 	{
 		return TYPE;
 	}
-
-	/*
-	 * 
-	 */
-	@Override
 	protected void dispatch(AddInstanceEventHandler handler)
 	{
 		handler.onAddInstance(this);
