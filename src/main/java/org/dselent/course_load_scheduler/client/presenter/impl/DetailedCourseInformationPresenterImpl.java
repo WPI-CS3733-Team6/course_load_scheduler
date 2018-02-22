3c5d24dbebd2918f97df84f449024f42ad6fd02c
bobbyshome <bobbyshome@autoreg-070211.dyn.wpi.edu>
2018-02-21 22:02:15 -0500

    Fix DetailedCourseInformation

--- src/main/java/org/dselent/course_load_scheduler/client/presenter/impl/DetailedCourseInformationPresenterImpl.java
+++ src/main/java/org/dselent/course_load_scheduler/client/presenter/impl/DetailedCourseInformationPresenterImpl.java
@@ -1,8 +1,12 @@
 package org.dselent.course_load_scheduler.client.presenter.impl;
 
+import org.dselent.course_load_scheduler.client.action.GoToClassSearchAction;
+import org.dselent.course_load_scheduler.client.action.SubmitCourseToCartAction;
+import org.dselent.course_load_scheduler.client.event.GoToClassSearchEvent;
 //import org.dselent.course_load_scheduler.client.action.DetailedCourseInformationAction;
 //import org.dselent.course_load_scheduler.client.event.DetailedCourseInformationEvent;
 import org.dselent.course_load_scheduler.client.event.InvalidLoginEvent;
+import org.dselent.course_load_scheduler.client.event.SubmitCourseToCartEvent;
 import org.dselent.course_load_scheduler.client.presenter.BasePresenter;
 import org.dselent.course_load_scheduler.client.presenter.CourseRegistrationCartPresenter;
 import org.dselent.course_load_scheduler.client.presenter.DetailedCourseInformationPresenter;
@@ -42,10 +46,12 @@ public class DetailedCourseInformationPresenterImpl extends BasePresenterImpl im
 	@Override
 	public void bind()
 	{
+		/*
 		HandlerRegistration registration;
 		
 		registration = eventBus.addHandler(InvalidLoginEvent.TYPE, this);
 		eventBusRegistration.put(InvalidLoginEvent.TYPE, registration);
+		*/
 	}
 	//----------------------------------------
 	
@@ -73,4 +79,33 @@ public class DetailedCourseInformationPresenterImpl extends BasePresenterImpl im
 	{
 		this.parentPresenter = parentPresenter;
 	}
+	
+	@Override
+	public void submitToCart() {
+		if(!submitClickInProgress) {
+			submitClickInProgress = true;
+			view.getSubmitToCartButton().setEnabled(false);
+			
+			String courseName = view.getCourseName().getText();
+			
+			HasWidgets container = parentPresenter.getView().getViewRootPanel();
+			SubmitCourseToCartAction sca = new SubmitCourseToCartAction(courseName);
+			SubmitCourseToCartEvent sce = new SubmitCourseToCartEvent(sca, container);
+			eventBus.fireEvent(sce);
+		}
+	}
+	
+	@Override
+	public void returnToSearch() {
+		if(!submitClickInProgress) {
+			submitClickInProgress = true;
+			view.getReturnToSearchButton().setEnabled(false);
+			
+			HasWidgets container = parentPresenter.getView().getViewRootPanel();
+			GoToClassSearchAction gca = new GoToClassSearchAction();
+			GoToClassSearchEvent gce = new GoToClassSearchEvent(gca, container);
+			eventBus.fireEvent(gce);
+		}
+	}
+	
 }
