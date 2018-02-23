4c857d4e1556c6c54a2997383f6bdc0f5af0024c
drd387 <dannyd40@gmail.com>
2018-02-23 14:15:51 -0500

    fixed a parameter for courseSearch

--- src/main/java/org/dselent/course_load_scheduler/client/action/CourseSearchAction.java
+++ src/main/java/org/dselent/course_load_scheduler/client/action/CourseSearchAction.java
@@ -4,13 +4,11 @@ public class CourseSearchAction extends Action {
 	private Integer subject;
 	private Integer term;
 	private Integer level;
-	private String name;
 	
 	public CourseSearchAction(Integer department, Integer term, Integer level, String name) {
 		this.subject = department;
 		this.term = term;
 		this.level = level;
-		this.name = name;
 	}
 
 	public Integer getDepartment() {
@@ -25,13 +23,8 @@ public class CourseSearchAction extends Action {
 		return level;
 	}
 
-	public String getName() {
-		return name;
-	}
-
 	@Override
 	public String toString() {
-		return "CourseSearchAction [subject=" + subject + ", term=" + term + ", level=" + level
-				+ ", name=" + name + "]";
+		return "CourseSearchAction [subject=" + subject + ", term=" + term + ", level=" + level + "]";
 	}
 }
