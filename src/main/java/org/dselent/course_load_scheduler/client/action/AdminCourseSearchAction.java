56534a0c2225e367f8f837356a92f2c20a2d0f61
Panha <34078543+Avorent@users.noreply.github.com>
2018-02-23 13:50:51 -0500

    Revert "gave search courses some actions.events"
    This reverts commit e88c50e5a8326d5afa98e5ec5124896c4cea32f6.

--- src/main/java/org/dselent/course_load_scheduler/client/action/AdminCourseSearchAction.java
+++ src/main/java/org/dselent/course_load_scheduler/client/action/AdminCourseSearchAction.java
@@ -3,12 +3,10 @@ package org.dselent.course_load_scheduler.client.action;
 public class AdminCourseSearchAction extends Action {
 	private String subject;
 	private String term;
-	private String level;
 	
-	public AdminCourseSearchAction(String subject, String term, String level) {
+	public AdminCourseSearchAction(String subject, String term) {
 		this.subject = subject;
 		this.term = term;
-		this.level = level;
 	}
 	
 	public String getSubject() {
@@ -18,13 +16,9 @@ public class AdminCourseSearchAction extends Action {
 	public String getTerm() {
 		return term;
 	}
-	
-	public String getLevel() {
-		return level;
-	}
 
 	@Override
 	public String toString() {
-		return "AdminCourseSearchAction [subject=" + subject + ", term=" + term + ", level" + level + "]";
+		return "AdminCourseSearchAction [subject=" + subject + ", term=" + term + "]";
 	}
 }
--- src/main/java/org/dselent/course_load_scheduler/client/event_handler/AdminCourseSearchEventHandler.java
+++
@@ -1,9 +0,0 @@
-package org.dselent.course_load_scheduler.client.event_handler;
-
-import org.dselent.course_load_scheduler.client.event.SearchCoursesEvent;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface AdminCourseSearchEventHandler extends EventHandler{
-	public void search(SearchCoursesEvent evt);
-}
