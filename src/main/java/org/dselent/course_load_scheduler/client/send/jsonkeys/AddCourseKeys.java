748909a87e998bd112d3b6d6a91c18f88fb1be6f
drd387 <dannyd40@gmail.com>
2018-02-22 20:07:07 -0500

    some work on addCourseTranslator

---
+++ src/main/java/org/dselent/course_load_scheduler/client/send/jsonkeys/AddCourseKeys.java
@@ -0,0 +1,10 @@
+package org.dselent.course_load_scheduler.client.send.jsonkeys;
+
+public enum AddCourseKeys {
+	NUMBER,
+	NAME,
+	TYPE,
+	DESCRIPTION,
+	LEVEL,
+	DEPARTMENT;
+}
--- src/main/java/org/dselent/course_load_scheduler/client/translator/impl/AddCourseTranslatorImpl.java
+++ src/main/java/org/dselent/course_load_scheduler/client/translator/impl/AddCourseTranslatorImpl.java
@@ -2,16 +2,26 @@ package org.dselent.course_load_scheduler.client.translator.impl;
 
 import org.dselent.course_load_scheduler.client.action.AddCourseAction;
 import org.dselent.course_load_scheduler.client.action.ReceiveAddCourseAction;
+import org.dselent.course_load_scheduler.client.send.jsonkeys.AddCourseKeys;
 import org.dselent.course_load_scheduler.client.translator.ActionTranslator;
+import org.dselent.course_load_scheduler.client.utils.JSONHelper;
 
 import com.google.gwt.json.client.JSONObject;
 
-public class AddCourseTranslatorImpl implements ActionTranslator<AddCourseAction, ReceiveAddCourseAction>{
+public class AddCourseTranslatorImpl implements ActionTranslator<AddCourseAction, ReceiveAddCourseAction> {
 
 	@Override
-	public JSONObject translateToJson(AddCourseAction object) {
-		// TODO Auto-generated method stub
-		return null;
+	public JSONObject translateToJson(AddCourseAction action) {
+		JSONObject jsonObject = new JSONObject();
+		
+		JSONHelper.putStringValue(jsonObject, JSONHelper.convertKeyName(AddCourseKeys.NUMBER), action.getNumber());
+		JSONHelper.putStringValue(jsonObject, JSONHelper.convertKeyName(AddCourseKeys.NAME), action.getName());
+		JSONHelper.putStringValue(jsonObject, JSONHelper.convertKeyName(AddCourseKeys.TYPE), action.getType());
+		JSONHelper.putStringValue(jsonObject, JSONHelper.convertKeyName(AddCourseKeys.DESCRIPTION), action.getDescription());
+		JSONHelper.putIntValue(jsonObject, JSONHelper.convertKeyName(AddCourseKeys.LEVEL), action.getLevel());
+		JSONHelper.putStringValue(jsonObject, JSONHelper.convertKeyName(AddCourseKeys.DEPARTMENT), action.getDepartment());
+		
+		return jsonObject;
 	}
 
 	@Override
