657affcc7cfea9724a0bd5f9141f6a0c91a32274
drd387 <dannyd40@gmail.com>
2018-02-22 19:36:14 -0500

    added ReceiveAddCourseAction, Event, and handler

---
+++ src/main/java/org/dselent/course_load_scheduler/client/translator/impl/AddCourseTranslatorImpl.java
@@ -0,0 +1,23 @@
+package org.dselent.course_load_scheduler.client.translator.impl;
+
+import org.dselent.course_load_scheduler.client.action.AddCourseAction;
+import org.dselent.course_load_scheduler.client.action.ReceiveAddCourseAction;
+import org.dselent.course_load_scheduler.client.translator.ActionTranslator;
+
+import com.google.gwt.json.client.JSONObject;
+
+public class AddCourseTranslatorImpl implements ActionTranslator<AddCourseAction, ReceiveAddCourseAction>{
+
+	@Override
+	public JSONObject translateToJson(AddCourseAction object) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ReceiveAddCourseAction translateToAction(JSONObject json) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
