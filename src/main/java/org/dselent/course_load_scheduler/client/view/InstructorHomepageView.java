b65a5d4658a6ae4ae25014f46e7cb115cfaee2aa
drd387 <dannyd40@gmail.com>
2018-02-23 21:19:06 -0500

    removed a part of the ui we no longer support

--- src/main/java/org/dselent/course_load_scheduler/client/view/InstructorHomepageView.java
+++ src/main/java/org/dselent/course_load_scheduler/client/view/InstructorHomepageView.java
@@ -5,32 +5,16 @@ import org.dselent.course_load_scheduler.client.presenter.InstructorHomepagePres
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.LayoutPanel;
-import com.google.gwt.user.client.ui.ListBox;
-import com.google.gwt.user.client.ui.SplitLayoutPanel;
-import com.google.gwt.user.client.ui.TextBox;
 
 public interface InstructorHomepageView extends BaseView<InstructorHomepagePresenter> {
 	
 	public LayoutPanel getLayoutPanel();
 	public void setLayoutPanel(LayoutPanel layoutPanel);
-	public HorizontalPanel getHeaderPanel();
-	public void setHeaderPanel(HorizontalPanel headerPanel);
-	public SplitLayoutPanel getCourseInfoPanel();
-	public void setCourseInfoPanel(SplitLayoutPanel courseInfoPanel);
-	public LayoutPanel getBodyPanel();
-	public void setBodyPanel(LayoutPanel bodyPanel);
-	public ListBox getRegisteredCourseListBox();
-	public void setRegisteredCourseListBox(ListBox registeredCourseListBox);
-	public ListBox getPendingCourseListBox();
-	public void setPendingCourseListBox(ListBox pendingCourseListBox);
 	public HorizontalPanel getSelectPanel();
 	public Button getCurrentClassButton();
 	public Button getClassSearchButton();
 	public Button getAccountInfoButton();
 	public Button getCourseCartButton();
 	public Button getGenerateScheduleButton();
-	public TextBox getRegisteredHeaderBox();
-	public TextBox getPendingHeaderBox();
-	public TextBox getTitleBox();
 	
 }
--- src/main/java/org/dselent/course_load_scheduler/client/view/impl/InstructorHomepageViewImpl.java
+++ src/main/java/org/dselent/course_load_scheduler/client/view/impl/InstructorHomepageViewImpl.java
@@ -13,9 +13,6 @@ import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.HasWidgets;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.LayoutPanel;
-import com.google.gwt.user.client.ui.ListBox;
-import com.google.gwt.user.client.ui.SplitLayoutPanel;
-import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
 
 public class InstructorHomepageViewImpl extends BaseViewImpl<InstructorHomepagePresenter> implements InstructorHomepageView {
@@ -29,14 +26,6 @@ public class InstructorHomepageViewImpl extends BaseViewImpl<InstructorHomepageP
 	@UiField Button accountInfoButton;
 	@UiField Button courseCartButton;
 	@UiField Button facultyCourseMapButton;
-	@UiField HorizontalPanel headerPanel;
-	@UiField SplitLayoutPanel courseInfoPanel;
-	@UiField LayoutPanel bodyPanel;
-	@UiField TextBox registeredHeaderBox;
-	@UiField TextBox pendingHeaderBox;
-	@UiField ListBox registeredCourseListBox;
-	@UiField ListBox pendingCourseListBox;
-	@UiField TextBox titleBox;
 
 	interface InstructorHomepageUiBinder extends UiBinder<Widget, InstructorHomepageViewImpl> {
 	}
@@ -76,77 +65,7 @@ public class InstructorHomepageViewImpl extends BaseViewImpl<InstructorHomepageP
 		this.layoutPanel = layoutPanel;
 	}
 
-	/**
-	 * @return the headerPanel
-	 */
-	public HorizontalPanel getHeaderPanel() {
-		return headerPanel;
-	}
-
-	/**
-	 * @param headerPanel the headerPanel to set
-	 */
-	public void setHeaderPanel(HorizontalPanel headerPanel) {
-		this.headerPanel = headerPanel;
-	}
-
-	/**
-	 * @return the courseInfoPanel
-	 */
-	public SplitLayoutPanel getCourseInfoPanel() {
-		return courseInfoPanel;
-	}
-
-	/**
-	 * @param courseInfoPanel the courseInfoPanel to set
-	 */
-	public void setCourseInfoPanel(SplitLayoutPanel courseInfoPanel) {
-		this.courseInfoPanel = courseInfoPanel;
-	}
-
-	/**
-	 * @return the bodyPanel
-	 */
-	public LayoutPanel getBodyPanel() {
-		return bodyPanel;
-	}
-
-	/**
-	 * @param bodyPanel the bodyPanel to set
-	 */
-	public void setBodyPanel(LayoutPanel bodyPanel) {
-		this.bodyPanel = bodyPanel;
-	}
-
-	/**
-	 * @return the registeredCourseListBox
-	 */
-	public ListBox getRegisteredCourseListBox() {
-		return registeredCourseListBox;
-	}
-
-	/**
-	 * @param registeredCourseListBox the registeredCourseListBox to set
-	 */
-	public void setRegisteredCourseListBox(ListBox registeredCourseListBox) {
-		this.registeredCourseListBox = registeredCourseListBox;
-	}
-
-	/**
-	 * @return the pendingCourseListBox
-	 */
-	public ListBox getPendingCourseListBox() {
-		return pendingCourseListBox;
-	}
-
-	/**
-	 * @param pendingCourseListBox the pendingCourseListBox to set
-	 */
-	public void setPendingCourseListBox(ListBox pendingCourseListBox) {
-		this.pendingCourseListBox = pendingCourseListBox;
-	}
-
-	/**
+		/**
 	 * @return the selectPanel
 	 */
 	public HorizontalPanel getSelectPanel() {
@@ -188,26 +107,6 @@ public class InstructorHomepageViewImpl extends BaseViewImpl<InstructorHomepageP
 		return facultyCourseMapButton;
 	}
 
-	/**
-	 * @return the registeredHeaderBox
-	 */
-	public TextBox getRegisteredHeaderBox() {
-		return registeredHeaderBox;
-	}
-
-	/**
-	 * @return the pendingHeaderBox
-	 */
-	public TextBox getPendingHeaderBox() {
-		return pendingHeaderBox;
-	}
-
-	/**
-	 * @return the titleBox
-	 */
-	public TextBox getTitleBox() {
-		return titleBox;
-	}
 	
 	@UiHandler("currentClassButton")
 	void onCurrentClassButtonClicked(ClickEvent evt) {
