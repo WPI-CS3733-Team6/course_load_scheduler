c7c05f38d58a748713a7a2a268ef1c0a0ce653f2
Josue Contreras <josudaco@hotmail.com>
2018-02-25 02:12:53 -0500

    AccountInfoView UI fixed

--- src/main/java/org/dselent/course_load_scheduler/client/view/impl/AccountInfoViewImpl.java
+++ src/main/java/org/dselent/course_load_scheduler/client/view/impl/AccountInfoViewImpl.java
@@ -25,7 +25,25 @@ public class AccountInfoViewImpl extends BaseViewImpl<AccountInfoPresenter> impl
 	@UiField TextBox AccountInfoPgTitle;
 	@UiField LayoutPanel AccountInfoBaseLayoutPanel;
 	@UiField Button submitChangesButton;
+	@UiField FlexTable AccountInfoFlexTable = new FlexTable();
+	@UiField HTMLPanel FlexPanel;
 	
+	public FlexTable getAccountInfoFlexTable() {
+		return AccountInfoFlexTable;
+	}
+
+	public void setAccountInfoFlexTable(FlexTable accountInfoFlexTable) {
+		AccountInfoFlexTable = accountInfoFlexTable;
+	}
+
+	public HTMLPanel getFlexPanel() {
+		return FlexPanel;
+	}
+
+	public void setFlexPanel(HTMLPanel flexPanel) {
+		FlexPanel = flexPanel;
+	}
+
 	private PasswordTextBox currPassword;
 	private PasswordTextBox newPassword;
 	private PasswordTextBox repeatNewPassword;
@@ -34,6 +52,8 @@ public class AccountInfoViewImpl extends BaseViewImpl<AccountInfoPresenter> impl
 	private String userName;
 	private String email;
 	private Integer requirement;
+	
+	
 
 	public Button getSubmitChangesButton() {
 		return submitChangesButton;
@@ -64,8 +84,7 @@ public class AccountInfoViewImpl extends BaseViewImpl<AccountInfoPresenter> impl
 	
 	public AccountInfoViewImpl() {
 		initWidget(uiBinder.createAndBindUi(this));
-		
-	    FlexTable t = new FlexTable();
+	
 	    currPassword = new PasswordTextBox();
 	    newPassword = new PasswordTextBox();
 	    repeatNewPassword = new PasswordTextBox();
@@ -73,28 +92,29 @@ public class AccountInfoViewImpl extends BaseViewImpl<AccountInfoPresenter> impl
 	    phoneNumber = new IntegerBox();
 
 	    // Create the text fields (This table is a 10x10)
-	    t.setText(0, 0, "Your username is:");
-	    t.setText(0, 1, userName);
-	    t.setText(1, 0, "Your WPI email is:");
-	    t.setText(1, 1, email);
-	    t.setText(2, 0, "Your minimum teaching requirement (course hours) is:");
-	    t.setText(2, 1, requirement.toString());
-	    t.setText(6, 0, "Change password:");
-	    t.setText(8, 0, "Prefered email (optional):");
-	    t.setText(10, 0, "Phone number (optional):");
+	    AccountInfoFlexTable.setText(0, 0, "Your username is:");
+	    AccountInfoFlexTable.setText(0, 1, userName);
+	    AccountInfoFlexTable.setText(1, 0, "Your WPI email is:");
+	    AccountInfoFlexTable.setText(1, 1, email);
+	    AccountInfoFlexTable.setText(2, 0, "Your minimum teaching requirement (course hours) is:");
+//	    t.setText(2, 1, requirement.toString());
+	    AccountInfoFlexTable.setText(6, 0, "Change password:");
+	    AccountInfoFlexTable.setText(8, 0, "Prefered email (optional):");
+	    AccountInfoFlexTable.setText(10, 0, "Phone number (optional):");
 	    
 	    // Create the widgets
-	    t.setWidget(6, 2, currPassword);
-	    t.setWidget(6, 3, newPassword);
-	    t.setWidget(6, 4, repeatNewPassword);
+	    AccountInfoFlexTable.setWidget(6, 2, currPassword);
+	    AccountInfoFlexTable.setWidget(6, 3, newPassword);
+	    AccountInfoFlexTable.setWidget(6, 4, repeatNewPassword);
 	    
-	    t.setWidget(8, 2, preferedEmail);
-	    t.setWidget(10, 2, phoneNumber);
+	    AccountInfoFlexTable.setWidget(8, 2, preferedEmail);
+	    AccountInfoFlexTable.setWidget(10, 2, phoneNumber);
 	    
 	    // ...and set it's column span so that it takes up the whole row.
-	    t.getFlexCellFormatter().setColSpan(1, 0, 3);
+	    AccountInfoFlexTable.getFlexCellFormatter().setColSpan(1, 0, 3);
 
-	    RootPanel.get().add(t);
+	    RootPanel.get().add(AccountInfoFlexTable);
+	    //AccountInfoBaseLayoutPanel.add(AccountInfoFlexTable);
 	}
 
 	@Override
@@ -126,6 +146,75 @@ public class AccountInfoViewImpl extends BaseViewImpl<AccountInfoPresenter> impl
 	@Override
 	public void setRequirement(Integer requirement) {
 		this.requirement = requirement;
+	}
+
+	//
+	public static AccountInfoUiBinder getUiBinder() {
+		return uiBinder;
+	}
+
+	public static void setUiBinder(AccountInfoUiBinder uiBinder) {
+		AccountInfoViewImpl.uiBinder = uiBinder;
+	}
+
+	public HTMLPanel getRootPanel() {
+		return rootPanel;
+	}
+
+	public void setRootPanel(HTMLPanel rootPanel) {
+		this.rootPanel = rootPanel;
+	}
+
+	public TextBox getAccountInfoPgTitle() {
+		return AccountInfoPgTitle;
+	}
+
+	public void setAccountInfoPgTitle(TextBox accountInfoPgTitle) {
+		AccountInfoPgTitle = accountInfoPgTitle;
+	}
+
+	public LayoutPanel getAccountInfoBaseLayoutPanel() {
+		return AccountInfoBaseLayoutPanel;
+	}
+
+	public void setAccountInfoBaseLayoutPanel(LayoutPanel accountInfoBaseLayoutPanel) {
+		AccountInfoBaseLayoutPanel = accountInfoBaseLayoutPanel;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public Integer getRequirement() {
+		return requirement;
+	}
+
+	public void setSubmitChangesButton(Button submitChangesButton) {
+		this.submitChangesButton = submitChangesButton;
+	}
+
+	public void setCurrPassword(PasswordTextBox currPassword) {
+		this.currPassword = currPassword;
+	}
+
+	public void setNewPassword(PasswordTextBox newPassword) {
+		this.newPassword = newPassword;
+	}
+
+	public void setRepeatNewPassword(PasswordTextBox repeatNewPassword) {
+		this.repeatNewPassword = repeatNewPassword;
+	}
+
+	public void setPreferedEmail(TextBox preferedEmail) {
+		this.preferedEmail = preferedEmail;
+	}
+
+	public void setPhoneNumber(IntegerBox phoneNumber) {
+		this.phoneNumber = phoneNumber;
 	}
 	
 	
